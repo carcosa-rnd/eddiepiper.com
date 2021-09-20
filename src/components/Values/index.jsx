b007cb226e634df42b39a641c58154250ad85cbd
John Carlo Cunanan <codingjohn@outlook.com>
2021-09-20 17:26:31 +0800

    fix: Add circle background on values section

--- src/components/Values/index.jsx
+++ src/components/Values/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import clsx from "clsx";
 
 import Container from "../Layout/Container";
 import Card from "./Card";
@@ -9,6 +10,14 @@ import { values } from "../../eddiepiper.config";
 function Values() {
   return (
     <section id="Värden" className={styles.values}>
+      <div className={styles.bg}>
+        <div className={clsx(styles.circle, styles.circle_1)}></div>
+        <div className={clsx(styles.circle, styles.circle_2)}></div>
+        <div className={clsx(styles.circle, styles.circle_3)}></div>
+        <div className={clsx(styles.circle, styles.circle_4)}></div>
+        <div className={clsx(styles.circle, styles.circle_5)}></div>
+        <div className={clsx(styles.circle, styles.circle_6)}></div>
+      </div>
       <Container className={styles.container}>
         <h2 className={styles.title}>{values.title}</h2>
         <div className={styles.cards_wrapper}>
