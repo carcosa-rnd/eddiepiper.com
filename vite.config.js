655261cb4f6c435387b9a861274d3e89cfa6ea1a
John Carlo Cunanan <codingjohn@outlook.com>
2021-09-28 13:14:49 +0800

    fix: change viteconfig base path

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  base: "/eddiepiper.com/",
+  base: "/",
 });
