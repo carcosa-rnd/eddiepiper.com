654f51a1826c67b3831bdedc4ef04bc718471ca1
John Carlo Cunanan <codingjohn@outlook.com>
2021-10-15 11:35:09 +0800

    deployment

--- src/components/Layout/index.jsx
+++
@@ -1,8 +0,0 @@
-import clsx from "clsx";
-import React from "react";
-
-function Layout({ children, className }) {
-  return <main className={clsx(className)}>{children}</main>;
-}
-
-export default Layout;
